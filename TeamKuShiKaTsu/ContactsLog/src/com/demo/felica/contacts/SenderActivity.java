484b395426fc1d7f0cfb76d2d5f2ac128acba27b
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-11-18 17:11:09 +0000

    - KuShiKaTsu のリザルトコードを受けとり、Toast で表示するようにしました。

--- TeamKuShiKaTsu/ContactsLog/src/com/demo/felica/contacts/SenderActivity.java
+++ TeamKuShiKaTsu/ContactsLog/src/com/demo/felica/contacts/SenderActivity.java
@@ -10,10 +10,12 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SenderActivity extends Activity implements OnClickListener {
 
 	private static final String TAG = "FEL";
+	private static final int FELICA_ACTIVITY = 0;
 	private String mPhoneNumber;
 	private EditText mNameEdit;
 	private Button mSendButton;
@@ -51,9 +53,45 @@ public class SenderActivity extends Activity implements OnClickListener {
 			final Intent felicaIntent = new Intent("jp.andeb.kushikatsu.FELICA_INTENT");
 			felicaIntent.addCategory(Intent.CATEGORY_DEFAULT);
 			felicaIntent.putExtra("EXTRA_INTENT", realIntent);
-			felicaIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-					| Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-			startActivity(felicaIntent);
+			startActivityForResult(felicaIntent, FELICA_ACTIVITY);
 		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode != FELICA_ACTIVITY) {
+			return;
+		}
+		final Toast toast;
+		if (resultCode == RESULT_OK) {
+			toast = Toast.makeText(this, "FeliCa Push 送信成功。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == RESULT_CANCELED) {
+			toast = Toast.makeText(this, "送信をキャンセルしました。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5000) {
+			toast = Toast.makeText(this, "FeliCa Push 送信に失敗しました。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5001) {
+			toast = Toast.makeText(this, "リクエストのパラメータが不正です。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5002) {
+			toast = Toast.makeText(this, "FeliCa デバイスがみつかりません。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5003) {
+			toast = Toast.makeText(this, "FeliCa デバイスは使用中です。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5004) {
+			toast = Toast.makeText(this, "データが大きすぎます。",
+					Toast.LENGTH_SHORT);
+		} else if (resultCode == 5005) {
+			toast = Toast.makeText(this, "送信がタイムアウトしました。",
+					Toast.LENGTH_SHORT);
+		} else {
+			toast = Toast.makeText(this, "不明なエラーです: " + resultCode,
+					Toast.LENGTH_SHORT);
+		}
+		toast.show();
 	}
 }
