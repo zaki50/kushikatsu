8be8f77ce7c4240dba6fbe283aea76d4e9f3af40
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-11-18 01:57:27 +0000

    − FelicaUtil に、FelicaException がスローされた原因の判別メソッドを追加しました。

--- TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/util/FelicaUtil.java
+++ TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/util/FelicaUtil.java
@@ -15,9 +15,79 @@
  */
 package jp.andeb.kushikatsu.util;
 
+import static com.felicanetworks.mfc.FelicaException.ID_ILLEGAL_STATE_ERROR;
+import static com.felicanetworks.mfc.FelicaException.ID_UNKNOWN_ERROR;
+import static com.felicanetworks.mfc.FelicaException.TYPE_ALREADY_ACTIVATED;
+import static com.felicanetworks.mfc.FelicaException.TYPE_CURRENTLY_ACTIVATING;
+import static com.felicanetworks.mfc.FelicaException.TYPE_REMOTE_ACCESS_FAILED;
+
 import com.felicanetworks.mfc.FelicaException;
 
 public final class FelicaUtil {
+
+    /**
+     * 渡された {@link FelicaException} が、 MFC プロセスとの通信に失敗したことを意味して
+     * いるかどかを返します。
+     *
+     * <p>
+     * 主に以下の場合にスローされます。
+     * </p>
+     * <ul>
+     *  <li>FeliCa デバイスを搭載していない端末で実行した場合</li>
+     * </ul>
+     *
+     * @param e
+     * チェック対象の例外。
+     * @return
+     * MFC プロセスとの通信失敗を意味している場合は {@code true}、そうでない場合は
+     * {@code false} を返します。
+     */
+    public static boolean isMissingMfc(FelicaException e) {
+        if (e == null) {
+            return false;
+        }
+        final boolean result = e.getID() == ID_UNKNOWN_ERROR
+                && e.getType() == TYPE_REMOTE_ACCESS_FAILED;
+        return result;
+    }
+
+    /**
+     * 渡された {@link FelicaException} が、 既にアクティベート済みのためアクティベート要求に
+     * 失敗したことを意味しているかどかを返します。
+     *
+     * @param e
+     * チェック対象の例外。
+     * @return
+     * アクティベート済みによる失敗を意味している場合は {@code true}、そうでない場合は
+     * {@code false} を返します。
+     */
+    public static boolean isAlreadyActivated(FelicaException e) {
+        if (e == null) {
+            return false;
+        }
+        final boolean result = e.getID() == ID_ILLEGAL_STATE_ERROR
+                && e.getType() == TYPE_ALREADY_ACTIVATED;
+        return result;
+    }
+
+    /**
+     * 渡された {@link FelicaException} が、 現在アクティベート中のためアクティベート要求に
+     * 失敗したことを意味しているかどかを返します。
+     *
+     * @param e
+     * チェック対象の例外。
+     * @return
+     * 現在アクティベート中のための失敗を意味している場合は {@code true}、そうでない場合は
+     * {@code false} を返します。
+     */
+    public static boolean isCurrentlyActivating(FelicaException e) {
+        if (e == null) {
+            return false;
+        }
+        final boolean result = e.getID() == ID_ILLEGAL_STATE_ERROR
+                && e.getType() == TYPE_CURRENTLY_ACTIVATING;
+        return result;
+    }
 
     public static String toString(FelicaException e) {
         if (e == null) {
