c0453086ce11a769ef6dbb54caa641652e9838e2
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-11-25 12:16:59 +0000

    - ファイルコメントに copyright もろもろ表記するようにしました。 - フォーマッタかけました。

--- trunk/TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/PrefActivity.java
+++ trunk/TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/PrefActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2010 Android DEvelopers' cluB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
 package jp.andeb.kushikatsu;
 
 import android.content.pm.PackageInfo;
@@ -7,9 +24,9 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
 
 public class PrefActivity extends PreferenceActivity {
 
@@ -39,23 +56,28 @@ public class PrefActivity extends PreferenceActivity {
         } catch (NameNotFoundException e) {
         }
 
-        PreferenceScreen screenPref = (PreferenceScreen) findPreference(KEY_VERSION_NUMBER);
+        final PreferenceScreen screenPref;
+        screenPref = (PreferenceScreen) findPreference(KEY_VERSION_NUMBER);
         screenPref.setSummary(versionName);
 
         // サウンドパターンのリスナー登録
-        ListPreference soundPatternList = (ListPreference) findPreference(KEY_SOUND_PATTERN);
-        soundPatternList.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+        final ListPreference soundPatternList;
+        soundPatternList = (ListPreference) findPreference(KEY_SOUND_PATTERN);
+        soundPatternList
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(
+                            final Preference preference, final Object newValue) {
+                        CharSequence cs = (CharSequence) newValue;
+                        int id = getResources().getIdentifier(cs.toString(),
+                                "raw", getPackageName());
 
-            @Override
-            public boolean onPreferenceChange(final Preference preference, final Object newValue) {
-                CharSequence cs = (CharSequence) newValue;
-                int id = getResources().getIdentifier(cs.toString(), "raw", getPackageName());
-
-                MediaPlayer mediaPlayer = MediaPlayer.create(PrefActivity.this, id);
-                mediaPlayer.start();
-                mediaPlayer = null;
-                return true;
-            }
-        });
+                        MediaPlayer mediaPlayer = MediaPlayer.create(
+                                PrefActivity.this, id);
+                        mediaPlayer.start();
+                        mediaPlayer = null;
+                        return true;
+                    }
+                });
     }
 }
