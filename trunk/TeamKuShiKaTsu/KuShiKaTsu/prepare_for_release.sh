71dec797852375b8a2440279321fc46ae644afc5
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-01 07:58:11 +0000

    - ant で楽できるように依存ライブラリは libs 直下にも見えるようにしました

--- trunk/TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
+++ trunk/TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
@@ -5,7 +5,7 @@ set -x
 find src/ -name \*.java -exec sed -e 's/^import edu\.umd\.cs\.findbugs\.annotations\.[^;]*;$//' -e 's/@DefaultAnnotation(NonNull.class)//' -e 's/@CheckForNull//' -i "" {} \;
 
 # findbugs 関連の jar を classpath から取り除く
-sed -e 's/<classpathentry kind="lib" path="libs\/findbugs\/[^"]*"\/>//' -i "" .classpath
+sed -e 's/<classpathentry kind="lib" path="libs\/findbugs-[0-9.]*\.jar"\/>//' -e 's/<classpathentry kind="lib" path="libs\/jsr305-[0-9.]*\.jar"\/>//' -i "" .classpath
 
 # debuggable を false にする
 sed -e 's/android:debuggable="true"/android:debuggable="false"/' -i "" AndroidManifest.xml
