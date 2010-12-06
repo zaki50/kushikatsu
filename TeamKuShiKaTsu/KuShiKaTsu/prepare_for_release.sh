7eb8c0724c50b5dd9b722efe6c6ce31e5548a5f2
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-06 23:32:23 +0000

    - prepare_for_release.sh で、使わない jar を削除するようにしました。

--- TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
+++ TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
@@ -6,3 +6,7 @@ find src/ -name \*.java -exec sed -e 's/^import edu\.umd\.cs\.findbugs\.annotati
 
 # findbugs 関連の jar を classpath から取り除く
 sed -e 's/<classpathentry kind="lib" path="libs\/findbugs-[0-9.]*\.jar"\/>//' -e 's/<classpathentry kind="lib" path="libs\/jsr305-[0-9.]*\.jar"\/>//' -i "" .classpath
+
+# ant が参照してしまわないように使ってないライブラリを削除
+rm -f libs/findbugs-*.jar
+rm -f libs/jsr305-*.jar
