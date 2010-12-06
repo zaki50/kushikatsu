88470aa70addc7de7b98f741d6c57101a67dcc2c
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-06 11:33:32 +0000

    - tagging KuShiKaTsu-1.0.0 from trunk at r95

---
+++ tags/TeamKuShiKaTsu-1.0.0/KuShiKaTsu/src/jp/andeb/kushikatsu/PrefActivity.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2010 Android DEvelopers' cluB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+package jp.andeb.kushikatsu;
+
+import static jp.andeb.kushikatsu.util.MediaPlayerUtil.RELEASE_PLAYER_LISTENER;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.view.Window;
+
+/**
+ * 設定画面のための {@link Activity} です。
+ */
+public class PrefActivity extends PreferenceActivity {
+
+    /** サウンドモード */
+    public static final String KEY_SOUND_MODE = "sound_mode";
+    /** 着信音のボリュームを使用 */
+    public static final String KEY_SAME_RINGER_MODE = "same_ringer_mode";
+    /** 振動モード */
+    public static final String KEY_VIBRATION_MODE = "vibration_mode";
+    /** バージョン番号 */
+    public static final String KEY_VERSION_NUMBER = "version_number";
+    /** サウンドパターン */
+    public static final String KEY_SOUND_PATTERN = "sound_pattern";
+
+    /** 擬似デバイスモード */
+    public static final String KEY_MOCK_DEVICE_ENABLED = "mock_device_enabled";
+    /** 擬似デバイスリザルトコード */
+    public static final String KEY_MOCK_DEVICE_RESULT_CODE = "mock_device_result_code";
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_LEFT_ICON);
+        super.onCreate(savedInstanceState);
+        setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+                R.drawable.icon);
+
+        addPreferencesFromResource(R.xml.pref);
+
+
+        // バージョン情報設定
+        String versionName = "";
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo info = null;
+            info = pm.getPackageInfo(this.getPackageName(), 0);
+            versionName = info.versionName;
+        } catch (NameNotFoundException e) {
+        }
+
+        final PreferenceScreen screenPref;
+        screenPref = (PreferenceScreen) findPreference(KEY_VERSION_NUMBER);
+        screenPref.setSummary(versionName);
+
+        // サウンドパターンのリスナー登録
+        final ListPreference soundPatternList;
+        soundPatternList = (ListPreference) findPreference(KEY_SOUND_PATTERN);
+        soundPatternList
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(
+                            final Preference preference, final Object newValue) {
+                        CharSequence cs = (CharSequence) newValue;
+                        int id = getResources().getIdentifier(cs.toString(),
+                                "raw", getPackageName());
+
+                        MediaPlayer mediaPlayer = MediaPlayer.create(
+                                PrefActivity.this, id);
+                        mediaPlayer.start();
+                        mediaPlayer
+                                .setOnCompletionListener(RELEASE_PLAYER_LISTENER);
+                        mediaPlayer = null;
+                        return true;
+                    }
+                });
+    }
+}
---
+++ tags/TeamKuShiKaTsu-1.0.0/KuShiKaTsuTest/src/jp/andeb/kushikatsu/test/SendActivityTest.java
@@ -0,0 +1,37 @@
+package jp.andeb.kushikatsu.test;
+
+import jp.andeb.kushikatsu.SendActivity;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class SendActivityTest extends
+		ActivityInstrumentationTestCase2<SendActivity> {
+
+	SendActivity sendActivity;
+
+	public SendActivityTest() {
+		super("jp.andeb.kushikatsu", SendActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		setActivityInitialTouchMode(false);
+		sendActivity = getActivity();
+	}
+
+	public void testSample() throws Exception {
+		assertNotNull(sendActivity);
+
+		Intent intent = new Intent("com.demo.felica.contacts.RECEIVE");
+		intent.putExtra("name", "ほげ");
+		intent.putExtra("phone", "000-0000-0000");
+		Intent fIntent = new Intent("jp.andeb.kushikatsu.FELICA_INTENT");
+		fIntent.addCategory(Intent.CATEGORY_DEFAULT);
+		fIntent.putExtra("EXTRA_INTENT", intent);
+		fIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		sendActivity.startActivity(fIntent);
+
+	}
+
+}
