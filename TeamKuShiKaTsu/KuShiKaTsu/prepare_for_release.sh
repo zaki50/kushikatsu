bae7f6f1da56d1716bd5e457a5f060b874970aa2
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-06 16:44:26 +0000

    - ADT 8.0.0 対応のための修正。   android:debuggable の記述はADTが面倒みてくれるので削除しました。

--- TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
+++ TeamKuShiKaTsu/KuShiKaTsu/prepare_for_release.sh
@@ -6,6 +6,3 @@ find src/ -name \*.java -exec sed -e 's/^import edu\.umd\.cs\.findbugs\.annotati
 
 # findbugs 関連の jar を classpath から取り除く
 sed -e 's/<classpathentry kind="lib" path="libs\/findbugs-[0-9.]*\.jar"\/>//' -e 's/<classpathentry kind="lib" path="libs\/jsr305-[0-9.]*\.jar"\/>//' -i "" .classpath
-
-# debuggable を false にする
-sed -e 's/android:debuggable="true"/android:debuggable="false"/' -i "" AndroidManifest.xml
