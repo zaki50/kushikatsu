6de37d13f31170186f55e322971955d8e6fed7c4
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-07 09:09:48 +0000

    リリースビルド作成用の shell スクリプトを作成しました。

---
+++ trunk/TeamKuShiKaTsu/KuShiKaTsu/make_release_build.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#set -x
+
+force=0
+if [ x"$1" = x-f ]; then
+  force=1
+fi
+
+projectdir=$(dirname $0)
+echo "changing directory to ${projectdir}" >&2
+pushd "${projectdir}" > /dev/null
+
+echo "creating destination directory" >&2
+
+# 念のため android:versionName が含まれていることを確認
+if ! grep -q android:versionName AndroidManifest.xml; then
+  echo "failed to get version name. exit." >&2
+  popd > /dev/null
+  exit 1
+fi
+
+# android:versionName の記述からバージョンを取得する
+versionName=$(grep android:versionName AndroidManifest.xml | sed -e 's/.*android:versionName="\([0-9][0-9.]*[0-9]\)".*/\1/')
+if [ x"$versionName" = x ]; then
+  echo "failed to get version name. exit." >&2
+  popd > /dev/null
+  exit 1
+fi
+
+#ビルドした成果物の格納先
+dest="released_binaries/${versionName}"
+if [ -e "${dest}" ]; then
+  if [ $force -eq 1 ]; then
+    rm -rf "${dest}/*"
+  else
+    echo "destination directory already exists. Is versionName correct?" >&2
+    echo "add -f to arguments if you intend to overwrite." >&2
+    popd > /dev/null
+    exit 1
+  fi
+fi
+mkdir -p "${dest}"
+echo "destinationdirectory created: ${dest}" >&2
+
+echo "invoking 'ant clean'." >&2
+if ! ant clean; then
+  echo "failed to clean project. exit." >&2
+  popd > /dev/null
+  exit 1
+fi
+echo "invoking 'ant release'." >&2
+if ! ant release; then
+  echo "failed to build release binary. exit." >&2
+  popd > /dev/null
+  exit 1
+fi
+
+echo "copying artifacts to ${dest}" >&2
+cp -a bin/*-release.apk "${dest}/"
+cp -a bin/proguard "${dest}/"
+
+popd > /dev/null
+exit 0
