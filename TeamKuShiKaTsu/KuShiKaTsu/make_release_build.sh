9b461e68e2e584f9da7a8b87874a045378d2c13c
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-07 09:26:35 +0000

    - リリースビルド作成スクリプトのメッセージの言い回しの修正です

--- TeamKuShiKaTsu/KuShiKaTsu/make_release_build.sh
+++ TeamKuShiKaTsu/KuShiKaTsu/make_release_build.sh
@@ -42,14 +42,14 @@ fi
 mkdir -p "${dest}"
 echo "destinationdirectory created: ${dest}" >&2
 
-echo "invoking 'ant clean'. output is written in ant_clean.log" >&2
+echo "invoking 'ant clean'. output is written to ant_clean.log" >&2
 if ! ant clean > ant_clean.log; then
   echo "failed to clean project. exit." >&2
   cat ./ant_clean.log
   popd > /dev/null
   exit 1
 fi
-echo "invoking 'ant release'. output is written in ant_release.log" >&2
+echo "invoking 'ant release'. output is written to ant_release.log" >&2
 if ! ant release | tee ./ant_release.log | grep "Please enter"; then
   echo "failed to build release binary. exit." >&2
   cat ./ant_release.log
