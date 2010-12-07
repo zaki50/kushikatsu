eb0ac01f9d11ab85bc68fc297b1ea231ceb7d9a1
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-07 09:55:52 +0000

    - 署名失敗時にエラーとして扱われなかったのを修正しました。

--- trunk/TeamKuShiKaTsu/KuShiKaTsu/make_release_build.sh
+++ trunk/TeamKuShiKaTsu/KuShiKaTsu/make_release_build.sh
@@ -56,6 +56,7 @@ rm -f ./ant_clean.log ./ant_release.log
 echo "invoking 'ant clean'. output is written to ant_clean.log" >&2
 if ! ant clean > ant_clean.log; then
   cat ./ant_clean.log
+  echo "" >&2
   echo "failed to clean project. exit." >&2
   popd > /dev/null
   exit 1
@@ -65,7 +66,17 @@ fi
 echo "invoking 'ant release'. output is written to ant_release.log" >&2
 if ! ant release | tee ./ant_release.log | grep "Please enter"; then
   cat ./ant_release.log
-  echo "failed to build release binary. exit." >&2
+  echo "" >&2
+  echo "BUILD FAILED" >&2
+  popd > /dev/null
+  exit 1
+fi
+
+if [ ! -e bin/*-release.apk ]; then
+  # 署名失敗の場合はどうも ant 自体はエラー扱いになっていないので独自にチェック
+  cat ./ant_release.log
+  echo "" >&2
+  echo "BUILD FAILED" >&2
   popd > /dev/null
   exit 1
 fi
@@ -78,5 +89,7 @@ mv ant_release.log "${dest}/"
 rm -f ant_clean.log
 
 # おしまい
+echo "" >&2
+echo "BUILD SUCCESSFUL" >&2
 popd > /dev/null
 exit 0
