453b8b5d7d22c10bd686f3de66ef80a339ee2d51
makoto1975 <makoto1975@249dc22d-9eb7-050a-e657-150211e162b9>
2010-12-13 13:23:34 +0000

    - 設定画面からセルフテストを行えるようにしました。

--- trunk/TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/PrefActivity.java
+++ trunk/TeamKuShiKaTsu/KuShiKaTsu/src/jp/andeb/kushikatsu/PrefActivity.java
@@ -18,18 +18,26 @@
 package jp.andeb.kushikatsu;
 
 import static jp.andeb.kushikatsu.util.MediaPlayerUtil.RELEASE_PLAYER_LISTENER;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.andeb.kushikatsu.helper.KushikatsuHelper;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.view.Window;
+import android.widget.Toast;
 
 /**
  * 設定画面のための {@link Activity} です。
@@ -52,6 +60,27 @@ public class PrefActivity extends PreferenceActivity {
     /** 擬似デバイスリザルトコード */
     public static final String KEY_MOCK_DEVICE_RESULT_CODE = "mock_device_result_code";
 
+    /** セルフテスト */
+    public static final String KEY_SELFTEST_MESSAGE_TYPE = "selftest_message_type";
+
+    /** インテント送信要求のリクエストコード */
+    private static final int SEND_REQUEST_CODE = 1;
+
+    private Map<CharSequence, Intent> selfTestMap_;
+    private Map<CharSequence, Intent> buildSelftestMap() {
+        final Map<CharSequence, Intent> map = new HashMap<CharSequence, Intent>();
+        map.put("intent", KushikatsuHelper
+                .buildIntentForSendIntent(KushikatsuHelper
+                        .buildIntentForKushikatsuInstall()));
+        map.put("browser",
+                KushikatsuHelper.buildIntentForStartBrowser(
+                        getString(R.string.site_url).toString(), null));
+        map.put("mailer", KushikatsuHelper.buildIntentForStartMailer(null,
+                null, null, null, null));
+
+        return map;
+    }
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_LEFT_ICON);
@@ -59,6 +88,8 @@ public class PrefActivity extends PreferenceActivity {
         setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, R.drawable.icon);
 
         addPreferencesFromResource(R.xml.pref);
+
+        selfTestMap_ = buildSelftestMap();
 
         // バージョン情報設定
         String versionName = "";
@@ -95,5 +126,107 @@ public class PrefActivity extends PreferenceActivity {
                         return true;
                     }
                 });
+
+        final CheckBoxPreference mockDeviceEnabledBox;
+        mockDeviceEnabledBox = (CheckBoxPreference) findPreference(KEY_MOCK_DEVICE_ENABLED);
+        mockDeviceEnabledBox
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference,
+                            Object newValue) {
+                        final ListPreference selftestTypeList;
+                        selftestTypeList = (ListPreference) findPreference(KEY_SELFTEST_MESSAGE_TYPE);
+                        selftestTypeList.setEnabled(!((Boolean) newValue)
+                                .booleanValue());
+                        return true;
+                    }
+                });
+        final boolean mockDeviceEnabled = mockDeviceEnabledBox.isChecked();
+
+        final ListPreference selftestTypeList;
+        selftestTypeList = (ListPreference) findPreference(KEY_SELFTEST_MESSAGE_TYPE);
+        selftestTypeList
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(
+                            final Preference preference, final Object newValue) {
+                        CharSequence cs = (CharSequence) newValue;
+
+                        final Intent i = selfTestMap_.get(cs);
+                        if (i == null) {
+                            Toast.makeText(PrefActivity.this,
+                                    R.string.failed_to_send, Toast.LENGTH_LONG)
+                                    .show();
+                        } else {
+                            KushikatsuHelper
+                                    .startKushikatsuForResult(
+                                            PrefActivity.this, i,
+                                            SEND_REQUEST_CODE, -1);
+                        }
+                        return true;
+                    }
+                });
+        selftestTypeList.setEnabled(!mockDeviceEnabled);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != SEND_REQUEST_CODE) {
+            return;
+        }
+
+        final Toast toast;
+        switch (resultCode) {
+        case KushikatsuHelper.RESULT_OK:
+            toast = Toast.makeText(this, R.string.send_result_ok,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_CANCELED:
+            toast = Toast.makeText(this, R.string.send_result_canceled,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_UNEXPECTED_ERROR:
+            toast = Toast.makeText(this, R.string.send_result_unexpected_error,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_INVALID_EXTRA:
+            toast = Toast.makeText(this, R.string.send_result_invalid_extra,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_DEVICE_NOT_FOUND:
+            toast = Toast.makeText(this, R.string.send_result_device_not_found,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_DEVICE_IN_USE:
+            toast = Toast.makeText(this, R.string.send_result_device_in_use,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_TOO_BIG:
+            toast = Toast.makeText(this, R.string.send_result_too_big,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_TIMEOUT:
+            toast = Toast.makeText(this, R.string.send_result_timeout,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_NOT_INITIALIZED:
+            toast = Toast.makeText(this, R.string.send_result_not_initialized,
+                    Toast.LENGTH_SHORT);
+            break;
+        case KushikatsuHelper.RESULT_DEVICE_LOCKED:
+            toast = Toast.makeText(this, R.string.send_result_device_locked,
+                    Toast.LENGTH_SHORT);
+            break;
+        default:
+            toast = Toast.makeText(this,
+                    getString(R.string.send_result_unknown_error, resultCode),
+                    Toast.LENGTH_SHORT);
+            break;
+        }
+        toast.show();
+    }
+
+
 }
